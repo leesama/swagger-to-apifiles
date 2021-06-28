be857d2c546a389f95caf9a8fced49677f364ae6
leesher <784852885@qq.com>
2021-06-28 13:21:37 +0800

    upd: 将返回值中的枚举生成为对象类型

--- src/index.ts
+++ src/index.ts
@@ -13,23 +13,24 @@ async function main() {
     const { swaggerUrl: url, outDir, serviceFileDir } = getConfig();
     const schemaData = await getSchemaData(url);
     const openapiTSData = openapiTS(schemaData);
-
+    // 生成请求类型
     outputFileSync(path.resolve(outDir), openapiTSData);
-    /** 正则匹配出枚举数据用于工具泛型生成枚举类型 */
-    const openapiTSDataSourceData = openapiTSData
-      .replace(/[\r\n]/g, "")
-      .replace(/\s+/g, "");
-    const openapiEnumTSData = [
-      ...new Set([
-        ...(openapiTSDataSourceData.match(/(?<=(\?:\|)).*?"(?=;)/g) || []),
-        ...(openapiTSDataSourceData.match(/(?<=(\?:))".*?"(?=;)/g) || []),
-      ]),
-    ]
-      .map((i) => `(${i})`)
-      .join("|");
+    // 生成返回值类型  用于生成正确的枚举
+    const responseTSData = openapiTSData.replace(
+      /(?<=(\| |: |\())(".*?")/g,
+      (text) => {
+        return `
+        {
+          name: ${text};
+          message: string;
+          code: ${text};
+        }`;
+      }
+    );
+    outputFileSync(path.resolve("src/types/response.d.ts"), responseTSData);
+    // 生成工具泛型
     ejs.renderFile(
       path.join(__dirname, "./templates/type.ejs"),
-      { data: openapiEnumTSData },
       function (err, str) {
         if (err) {
           console.log(err);
