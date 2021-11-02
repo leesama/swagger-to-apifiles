ec0711aa5ab210aa3bba25615d5722925daa2f4b
lee <784852885@qq.com>
2021-11-02 15:07:51 +0800

    upd: commit

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
 #!/usr/bin/env node
 
-import path from "path";
-import { outputFileSync } from "fs-extra";
-import openapiTS, { SchemaObject } from "openapi-typescript";
 import getConfig from "./getConfig";
-import makeServiceFile from "./requestTemplateMaker";
-import getSchemaData from "./getSchemaData";
-
 import makeFiles from "./makeFiles";
-
 async function main() {
   try {
     const { generateConfig } = getConfig();
